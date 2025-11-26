bbfcbedf29f31978d3b3c75df68e40ad00c6eefa
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2025-11-25 18:54:51 -0700

    pulumi deploy

---
+++ apps/notifications-service/pulumi/index.ts
@@ -0,0 +1,42 @@
+import { readFileSync } from 'fs';
+import * as k8s from '@pulumi/kubernetes';
+import {
+  Outputs,
+  getConfig,
+  deployApi,
+  createSecret,
+  DeployApiArgs,
+} from '@shapeshiftoss/unchained-pulumi';
+
+//https://www.pulumi.com/docs/intro/languages/javascript/#entrypoint
+export = async (): Promise<Outputs> => {
+  const appName = 'shapeshift';
+  const coinstack = 'notifications-service';
+
+  const { kubeconfig, config, namespace } = await getConfig();
+  const sampleEnv = readFileSync('../../../.env.example');
+
+  const assetName = config.assetName;
+  const provider = new k8s.Provider('kube-provider', { kubeconfig });
+
+  createSecret({ name: assetName, env: sampleEnv, namespace }, { provider });
+
+  const docker: DeployApiArgs['docker'] = {
+    context: '../../../',
+    tag: 'test1',
+    command: ['sh', '-c', 'yarn db:migrate && yarn start:prod'],
+  };
+
+  await deployApi({
+    appName,
+    assetName,
+    coinstack,
+    config,
+    docker,
+    namespace,
+    provider,
+    sampleEnv,
+  });
+
+  return {};
+};
